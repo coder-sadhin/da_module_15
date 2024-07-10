86153ee31c6634061fb289718d7456e3c4a2cf60
akkas_dev <codersadhin@gmail.com>
2024-07-10 23:26:47 +0800

    reverse queue change code

--- reverse_queue.cpp
+++ reverse_queue.cpp
@@ -15,3 +15,21 @@ queue<int> reverseQueue(queue<int> q)
     }
     return q;
 }
+
+
+// queue<int> reverseQueue(queue<int> q)
+// {
+//     // Write your code here.
+//     stack<int> s;
+//     while (!q.empty())
+//     {
+//         s.push(q.front());
+//         q.pop();
+//     }
+//     while (!s.empty())
+//     {
+//         q.push(s.top());
+//         s.pop();
+//     }
+//     return q;
+// }
