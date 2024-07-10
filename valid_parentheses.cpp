65efd5ae5ade9edf561f1491d5f98442c59000a8
akkas_dev <codersadhin@gmail.com>
2024-07-10 23:24:56 +0800

    valide parentheses change code

--- valid_parentheses.cpp
+++ valid_parentheses.cpp
@@ -37,6 +37,40 @@ public:
                 }
             }
         }
+
+        // {
+        //     if (c == '(' || c == '{' || c == '[')
+        //     {
+        //         st.push(c);
+        //     }
+        //     else
+        //     {
+        //         if (st.empty())
+        //         {
+        //             return false;
+        //         }
+        //         else
+        //         {
+        //             if (c == ')' && st.top() == '(')
+        //             {
+        //                 st.pop();
+        //             }
+        //             else if (c == '}' && st.top() == '{')
+        //             {
+        //                 st.pop();
+        //             }
+        //             else if (c == ']' && st.top() == '[')
+        //             {
+        //                 st.pop();
+        //             }
+        //             else
+        //             {
+        //                 return false;
+        //             }
+        //         }
+        //     }
+        // }
+
         return st.empty();
     }
 };
